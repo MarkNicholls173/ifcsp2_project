6eb0dd1c90751bbbb317f6eeec5e314751d3659f
Mark Nicholls <mark.nicholls@dvsa.gov.uk>
2022-06-22 14:02:30 +0100

    add salary, change font_helv to font_ems, add font_ems_bold

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ class EmployeeManagementGUI(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        font_helv = ('Helvetica', 12)
+        # Other variables
+        font_ems = ('Helvetica', 12)
+        font_ems_bold = font_ems + ('bold',)
 
-
-        # Define Variables
+        # Define Class Variables
         self.first_name = tk.StringVar()
         self.last_name = tk.StringVar()
         self.position = tk.StringVar()
@@ -27,16 +28,29 @@ class EmployeeManagementGUI(tk.Tk):
         main_frame.grid(row=0, column=0, columnspan=4)
 
         # First Name
-        lbl_first_name = tk.Label(main_frame, text='First Name', font=font_helv)
+        lbl_first_name = tk.Label(main_frame, text='First Name', font=font_ems)
         lbl_first_name.grid(row=0, column=0, padx=10, pady=10, sticky='w')
-        self.text_first_name = tk.Entry(main_frame, textvariable=self.first_name, font=font_helv)
+        self.text_first_name = tk.Entry(main_frame, textvariable=self.first_name, font=font_ems)
         self.text_first_name.grid(row=0, column=1, padx=10, pady=10, sticky='w')
 
         # Last Name
-        lbl_last_name = tk.Label(main_frame, text='Last Name', font=font_helv)
-        lbl_last_name.grid(row=0, column=3, padx=10, pady=10, sticky='w')
-        self.text_last_name = tk.Entry(main_frame, textvariable=self.last_name, font=font_helv)
-        self.text_last_name.grid(row=0, column=4, padx=10, pady=10, sticky='w')
+        lbl_last_name = tk.Label(main_frame, text='Last Name', font=font_ems)
+        lbl_last_name.grid(row=0, column=2, padx=10, pady=10, sticky='w')
+        self.text_last_name = tk.Entry(main_frame, textvariable=self.last_name, font=font_ems)
+        self.text_last_name.grid(row=0, column=3, padx=10, pady=10, sticky='w')
+
+        # Position
+        lbl_position = tk.Label(main_frame, text='Position', font=font_ems)
+        lbl_position.grid(row=1, column=0, padx=10, pady=10, sticky='w')
+        self.text_position = tk.Entry(main_frame, textvariable=self.position, font=font_ems)
+        self.text_position.grid(row=1, column=1, padx=10, pady=10, sticky='w')
+
+        # Salary
+        lbl_salary = tk.Label(main_frame, text='Salary', font=font_ems)
+        lbl_salary.grid(row=1, column=2, padx=10, pady=10, sticky='w')
+        self.text_salary = tk.Entry(main_frame, textvariable=self.salary, font=font_ems)
+        self.text_salary.grid(row=1, column=3, padx=10, pady=10, sticky='w')
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
