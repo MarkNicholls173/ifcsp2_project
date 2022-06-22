7dc74b6351fca29d556837acdf46be42007efe0a
Mark Nicholls <mark.nicholls@dvsa.gov.uk>
2022-06-22 13:51:07 +0100

    add first name

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ class EmployeeManagementGUI(tk.Tk):
     # constructor
     def __init__(self):
         super().__init__()
-        
+
+        font_helv = ('Helvetica', 12)
+
 
         # Define Variables
         self.first_name = tk.StringVar()
@@ -16,8 +18,8 @@ class EmployeeManagementGUI(tk.Tk):
         self.salary = tk.StringVar()
 
         # Window Setup
-        self.title("Employee Management System") # can i add employee id here?
-        self.geometry("800x600+600+100")
+        self.title('Employee Management System')  # can i add employee id here?
+        self.geometry('800x600+600+100')
 
         # Widgets
         # Main Frame for ....
@@ -25,7 +27,10 @@ class EmployeeManagementGUI(tk.Tk):
         main_frame.grid(row=0, column=0, columnspan=4)
 
         # First Name
-        lbl_first_name = tk.Label(main_frame, text="First Name")
+        lbl_first_name = tk.Label(main_frame, text='First Name', font=font_helv)
+        lbl_first_name.grid(row=0, column=0, padx=10, pady=10, sticky='w')
+        self.text_first_name = tk.Entry(main_frame, textvariable=self.first_name, font=font_helv)
+        self.text_first_name.grid(row=0, column=1, padx=10, pady=10, sticky='w')
 
 
 # Press the green button in the gutter to run the script.
